a0a5043cecc8b254a77ae24f643916836999b76c
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2025-03-18 19:16:40 +0200

    feat: Colored Shapes alignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#EFECEA',
-      },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+  }
